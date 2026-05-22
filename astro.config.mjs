25f4f60dfe81fbfbcbd123724d9bbef9828791ab
CSUlyc <1138851994@qq.com>
2026-05-22 23:39:10 +0800

    忽略doc文件夹

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,19 +9,14 @@ export default defineConfig({
 			title: 'Graduate Blog',
 			description: 'A blog about my graduate school journey.',
 			sidebar: [
+				{ slug: 'index', label: '首页' },
 				{
 					label: 'D2L 学习笔记',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ slug: 'index', label: '首页' },
-						{ slug: '04-数据操作与预处理', label: '04-数据操作与预处理' },
-					],
+					items: [{ autogenerate: { directory: 'project1' } }],
 				},
 				{
 					label: '项目展示',
-					items: [
-						{ label: 'D2L / RNN 实验探索', slug: 'projects/d2l-rnn' },
-					],
+					items: [{ autogenerate: { directory: 'projects' } }],
 				},
 			],
 		}),
