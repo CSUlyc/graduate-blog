56eb5a8248918ea5f05466439dfe77a4a5b96400
CSUlyc <1138851994@qq.com>
2026-05-27 17:32:40 +0800

    注意力机制10.1-10.3

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,20 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 // https://astro.build/config
 export default defineConfig({
+	markdown: {
+		remarkPlugins: [remarkMath],
+		rehypePlugins: [rehypeKatex],
+	},
 	integrations: [
 		starlight({
 			title: 'Graduate Blog',
 			description: 'A blog about my graduate school journey.',
+			customCss: ['katex/dist/katex.min.css'],
 			sidebar: [
 				{ slug: 'index', label: '首页' },
 				{
